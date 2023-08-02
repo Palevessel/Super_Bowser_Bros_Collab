406702da8e47e21025882d0c76b228b66543c304
buffpig <89503895+buffpig@users.noreply.github.com>
2023-08-02 16:28:49 -0700

    currently adding nukes (:

---
+++ super bowzer bro/Assets/findenemies.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class findenemies : MonoBehaviour
+{
+    public LayerMask enemymask;
+    public Transform target;
+    public float targetingrange = 5f;
+    public float cooldown = 10f;
+    public float realcooldown = 10f;
+    public float enemydebugger;
+    public bool shouldkaboom;
+    public GameObject angel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        findtarget();
+
+        if (cooldown > 0f)
+        {
+            cooldown -= Time.deltaTime;
+        }
+    }
+    void findtarget()
+    {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetingrange, (Vector2)transform.position, 0f, enemymask);
+        if (hits.Length > 0)
+        {
+            target = hits[0].transform;
+        }
+        if(hits.Length >= 5 && cooldown <= 0f)
+        {
+            boom();
+            cooldown = realcooldown;
+        }
+        enemydebugger = hits.Length;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, targetingrange);
+    }
+
+    public void boom()
+    {
+        
+    }
+}
